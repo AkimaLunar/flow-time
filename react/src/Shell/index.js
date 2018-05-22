d9de04a3476c111877fcca5d4e0914aa7a4d0389
Ria Carmin <akimalunar@gmail.com>
2018-05-22 10:17:56 -0700

    Switched  from loaded-components to react-loadable

--- react/src/Shell/index.js
+++ react/src/Shell/index.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import { hot } from 'react-hot-loader'
-import loadable from 'loadable-components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader';
+import Loadable from 'react-loadable';
 
-import Loading from '../Loading'
-import Footer from '../Footer'
+import Loading from '../Loading';
+import Footer from '../Footer';
 
-const Home = loadable(
-  () => import(/* webpackChunkName:'Home' */ '../Home'),
-  {
-    LoadingComponent: Loading
-  }
-);
+const Home = Loadable({
+    loader: () =>
+        import(/* webpackChunkName:'Home', webpackPrefetch: true */ '../Home'),
+    loading: Loading
+});
 
-const About = loadable(
-  () => import(/* webpackChunkName:'About' */ '../About'),
-  {
-    LoadingComponent: Loading
-  }
-);
+const About = Loadable({
+    loader: () =>
+        import(/* webpackChunkName:'About', webpackPrefetch: true */ '../About'),
+    loading: Loading
+});
 
 const Shell = () => (
-  <Router>
-    <main>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route component={Home} />
-      </Switch>
-      <Footer />
-    </main>
-  </Router>
-)
+    <Router>
+        <main>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route component={Home} />
+            </Switch>
+            <Footer />
+        </main>
+    </Router>
+);
 
-export default hot(module)(Shell)
+export default hot(module)(Shell);
